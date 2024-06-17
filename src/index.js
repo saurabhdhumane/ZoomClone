2bda100fd9a3eb95683e2322d661b8f2ad18e368
Saurabh Dhumne <saurabhdhumneraje@gmail.com>
2024-06-17 15:54:10 +0530

    Deploy to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router basename="/ZoomClone">
+    <App />
+  </Router>,
+  document.getElementById('root')
 
   </React.StrictMode>
 );
